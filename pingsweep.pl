b391b029daea1c999e3bf6362f85e235bd51a598
dudarchikconnor <36935419+dudarchikconnor@users.noreply.github.com>
2018-03-05 18:59:41 -0500

    IP Format Validation

--- pingsweep.pl
+++ pingsweep.pl
@@ -10,15 +10,23 @@ if ($#ARGV != 0) # If more/less than 1 cmd line arg
 	print ("pingsweep.pl 10.10.1.1-10.10.2.50\n");
 	print ("pingsweep.pl 10.10.0.0/16\n");
 }
-if (index($ARGV[0], "-" != -1)) # If the arg contains a -
+if (index($ARGV[0], "-") != -1) # If the arg contains a -
 {
-	print ("IP Range\n");
+	my @ips = split /-/, $ARGV[0]; # Get the IPs
+	my @ipone = split /\./, $ips[0]; # Separate first IP into octets
+	my @iptwo = split /\./, $ips[1]; # Separate second IP into octets
+	#foreach $x (ipone)
+	#{
+		
 }
-if (index($ARGV[0], "\/" != -1)) # If the arg contains a /
+elsif (index($ARGV[0], "\/") != -1) # If the arg contains a /
 {
 	print ("CIDR Notation\n");
 }
 else
 {
-	print ("Failure. Like me.\n");
+	print ("Incorrect argument format.\n");
+	print ("Example usages:\n");
+	print ("pingsweep.pl 10.10.1.1-10.10.2.50\n");
+	print ("pingsweep.pl 10.10.0.0/16\n");
 }
