677b77e1328406a29c71e32509ef122b8febf4df
axl8178 <axl8178@rit.edu>
2018-03-05 21:52:09 -0500

    Another updated to sniffer tool
    The filtering for port and packet type is added and working far as SEC LAB computer goes. I'll need to comment and try to add more if I can.

--- Sniffer.py
+++ Sniffer.py
@@ -6,52 +6,53 @@ if(len(interface)==0):
 	print "No interface specified, exiting..."
 	sys.exit(1)
 
-#filterType = raw_input(">>>Enter packet type to filter: ")
-#ports      = raw_input(">>>Enter port to monitor for  : ")
+portOrType = raw_input("Type packet or port to filter for them, press enter if no filter: ")
+if (portOrType.upper() == "PACKET"):
+	filtering = ""
+	count = 0
+	print ">>>Enter packet type to filter for, or enter to move on:"
+	while(True):
+		filType = raw_input(">>>")
+		if(len(filType)>0):
+			if(count > 0):
+				filtering += " and "
+			filtering += filType
+			count+=1
+		else:
+			break
 
-filterType = []
-count = 0
-print ">>>Enter packet type to filter for, or enter to move on:"
-while(True):
-	filType = raw_input(">>>")
-	if(len(filType)>0):
-		if(count > 0):
-			filterType.append(" and ")
-		filterType.append(filType)
-		count+=1
-	else:
-		break
+elif(portOrType.upper() == "PORT"):
+	filtering = ""
+	count = 0
+	print ">>>Enter TCP ports to filter for, or enter to move on:"
+	while(True):
+		port = raw_input(">>>")
+		if(len(port)>0):
+			if(count == 0):
+				filtering += "tcp and ("
+			if(count > 0):
+				filtering += " or "
+			filtering += "port " + port
+			count+=1
+		else:
+			if(count > 0):
+				filtering += ")"
+			break
 
-ports = []
-count = 0
-print ">>>Enter TCP ports to filter for, or enter to move on:"
-while(True):
-	port = raw_input(">>>")
-	if(len(port)>0):
-		if(count == 0):
-			ports.append("tcp and (")
-		if(count > 0):
-			ports.append(" or ")
-		ports.append("port " + port)
-		count+=1
-	else:
-		if(count > 0):
-			ports.append(")")
-		break
+elif(portOrType.upper() != ""):
+	print "Invalid input, exiting..."
+	sys.exit(1)
 
-duration   = int(raw_input(">>>Enter duration of search (default=5)  : "))
+else:
+	filtering = ""
+
+print filtering
+duration = raw_input(">>>Enter duration of search (default=5): ")
 if(duration == ""):
 	duration = 5
-	
-completeFilter=""
-for filType in filterType:
-	completeFilter += filType
-if(len(ports) > 0):
-	completeFilter += " and "
-for port in ports:
-	completeFilter += port
-print completeFilter	
+else:
+	duration = int(duration)
 
 print "\n[*] Sniffing"
-packets = sniff(iface=interface, filter=completeFilter, timeout=duration, count=0, prn=lambda x: x.summary())
+packets = sniff(iface=interface, filter=filtering, timeout=duration, count=0, prn=lambda x: x.summary())
 wrpcap('packets.pcap', packets)
